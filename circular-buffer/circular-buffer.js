4941b4526247220f0f879e3e66e8df35599cbaa8
Ben Lewis <bennlewis@gmail.com>
2014-10-26 19:43:56 -0600

    complete circular buffer first iteration

--- circular-buffer/circular-buffer.js
+++ circular-buffer/circular-buffer.js
@@ -1,62 +1,61 @@
-var bufferEmptyException = function() {
-  return new Error('Buffer is empty');
-};
-
-var bufferFullException = function() {
-  return new Error('Buffer is full');
-};
-
-Array.prototype.lastElement = function() {
-  return this[this.length - 1];
-};
-
 var CircularBuffer = function(size) {
-  if (!(this instanceof CircularBuffer)) {
-    return new CircularBuffer(size);
-  }
+  if (!(this instanceof CircularBuffer)) { return new CircularBuffer(size); }
   this.size = size;
   this.clear();
+};
+
+CircularBuffer.prototype.clear = function(datum) {
+  this.readHead = 0;
+  this.writeHead = 0;
+  this.data = new Array(this.size);
 };
 
 CircularBuffer.prototype.read = function() {
   if (!this.data) { throw bufferEmptyException(); }
   var datum = this.data[this.readHead];
   this.data[this.readHead] = null;
-  this.readHead = (this.readHead + 1) % this.size;
+  this._advanceReadHead();
   return datum;
 };
 
 CircularBuffer.prototype.write = function(datum) {
-  if (this._isFull()) {
-    throw bufferFullException();
-  }
+  if (this.isFull()) { throw bufferFullException(); }
   this._update(datum);
 };
 
 CircularBuffer.prototype.forceWrite = function(datum) {
   this._update(datum);
-  if (this.data.length === this.size) {
-    this.readHead = (this.readHead + 1) % this.size;
-  }
+  if (this.isFull()) { this._advanceReadHead(); }
+};
+
+CircularBuffer.prototype.isFull = function() {
+  return this.data.filter(datumIsPresent).length === this.size;
 };
 
 CircularBuffer.prototype._update = function(datum) {
-  if (datum === null || datum === undefined) { return; }
+  if (!datumIsPresent(datum)) { return; }
   this.data[this.writeHead] = datum;
+  this._advanceWriteHead();
+};
+
+CircularBuffer.prototype._advanceReadHead = function() {
+  this.readHead = (this.readHead + 1) % this.size;
+};
+
+CircularBuffer.prototype._advanceWriteHead = function() {
   this.writeHead = (this.writeHead + 1) % this.size;
 };
 
-CircularBuffer.prototype._isFull = function() {
-  return this.data.filter(function(datum) {
-    return datum !== null && datum !== undefined;
-  }).length === this.size;
+function datumIsPresent(datum) {
+  return datum !== null && datum !== undefined;
+}
+
+var bufferEmptyException = function() {
+  return new Error('Buffer is empty');
 };
 
-
-CircularBuffer.prototype.clear = function(datum) {
-  this.readHead = 0;
-  this.writeHead = 0;
-  this.data = new Array(this.size);
+var bufferFullException = function() {
+  return new Error('Buffer is full');
 };
 
 module.exports = {
