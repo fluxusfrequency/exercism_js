6254fc666f1c70c5fa3d6db34a3e439e8c634d2d
Ben Lewis <bennlewis@gmail.com>
2014-10-29 20:27:27 -0600

    complete simple cipher

--- simple-cipher/simple-cipher.js
+++ simple-cipher/simple-cipher.js
@@ -1,5 +1,17 @@
-var Cipher = function() {
-  this.key = 'aaaaaaaaaa';
+function generateAlphabet() {
+  var letters = [];
+  for (var i = 'a'.charCodeAt(0); i <= 'z'.charCodeAt(0); i++) {
+    letters.push(String.fromCharCode(i));
+  }
+  return letters;
+};
+
+var ALPHABET = generateAlphabet();
+
+var Cipher = function(key) {
+  this._validateKey(key);
+  this.key = key || 'aaaaaaaaaa';
+  this.keyDistances = this._keyDistances();
 };
 
 Cipher.prototype.encode = function(message) {
@@ -10,13 +22,25 @@ Cipher.prototype.decode = function(message) {
   return this._process(message, 'decode');
 };
 
-Cipher.prototype._process = function(message, type) {
-  var rotated = [],
-      chars = message.split(''),
-      distances = this._keyDistances();
-  for(var i = 0, max = chars.length; i < max; i++) {
-    var char = chars[i];
+Cipher.prototype._validateKey = function(key) {
+  if (key && key.toUpperCase() === key || key === '') {
+    throw(new Error('Bad key'));
   }
+};
+
+Cipher.prototype._process = function(message, type) {
+  var processed = [],
+      chars = message.split('');
+
+  for(var i = 0, max = chars.length; i < max; i++) {
+    var c = chars[i],
+        distance = this.keyDistances[i],
+        rotated = this._rotate(type, distance);
+
+    processed.push(rotated[c]);
+  }
+
+  return processed.join('');
 };
 
 Cipher.prototype._keyDistances = function() {
@@ -27,5 +51,22 @@ Cipher.prototype._calculateDistance = function(char) {
   return char.charCodeAt(0) - 'a'.charCodeAt(0);
 };
 
+Cipher.prototype._rotate = function(direction, distance) {
+  if (direction === 'encode') {
+    return this._rotateCharacters(distance);
+  } else {
+    return this._rotateCharacters(-distance);
+  }
+};
+
+Cipher.prototype._rotateCharacters = function(distance) {
+  var rotations = {};
+
+  for(var i = 0; i < 26; i++) {
+    var j = (i + distance) % 26;
+    rotations[ALPHABET[i]] = ALPHABET[j];
+  }
+  return rotations;
+};
 
 module.exports = Cipher;
