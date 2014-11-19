85c8a1fa850df67a46ab3a2a87648f1ba353ca31
Ben Lewis <bennlewis@gmail.com>
2014-11-19 06:49:09 -0700

    ocr complete

--- ocr-numbers/ocr-numbers.js
+++ ocr-numbers/ocr-numbers.js
@@ -1,34 +1,39 @@
-module.exports = {
-  OCR_MATRICES: [
-    [" ", "|", "|", "_", " ", "_", " ", "|", "|"],
-    [" ", " ", " ", " ", " ", " ", " ", "|", "|"],
-    [" ", " ", "|", "_", "_", "_", " ", "|", " "],
-    [" ", " ", " ", "_", "_", "_", " ", "|", "|"],
-    [" ", "|", " ", " ", "_", " ", " ", "|", "|"],
-    [" ", "|", " ", "_", "_", "_", " ", " ", "|"],
-    [" ", "|", "|", "_", "_", "_", " ", " ", "|"],
-    [" ", " ", " ", "_", " ", " ", " ", "|", "|"],
-    [" ", "|", "|", "_", "_", "_", " ", "|", "|"],
-    [" ", "|", " ", "_", "_", "_", " ", "|", "|"]
-  ],
+(function() {
+  Array.prototype.flatten = function() {
+    return Array.prototype.concat.apply([], this);
+  };
 
-  convert: function(ocrNumber) {
-    var split = ocrNumber.split('\n');
-    var converted = [];
-    for(var i = 0, max = split.length; i < max; i += 4) {
-      var line = split.slice(i, i + 4);
-      converted.push(this.ocr(line));
+  Array.prototype.equals = function(other) {
+    if(this === other) { return true; }
+    if(this == null || other == null) { return false; }
+    if(this.length !== other.length) { return false; }
+
+    for (var i = 0; i < this.length; ++i) {
+      if (this[i] !== other[i]) return false;
     }
-    return converted.join('');
-  },
+    return true;
+  };
 
-  ocr: function(line) {
+  var OCR_MATRICES = [
+    [' ', '|', '|', '_', ' ', '_', ' ', '|', '|'],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', '|', '|'],
+    [' ', ' ', '|', '_', '_', '_', ' ', '|', ' '],
+    [' ', ' ', ' ', '_', '_', '_', ' ', '|', '|'],
+    [' ', '|', ' ', ' ', '_', ' ', ' ', '|', '|'],
+    [' ', '|', ' ', '_', '_', '_', ' ', ' ', '|'],
+    [' ', '|', '|', '_', '_', '_', ' ', ' ', '|'],
+    [' ', ' ', ' ', '_', ' ', ' ', ' ', '|', '|'],
+    [' ', '|', '|', '_', '_', '_', ' ', '|', '|'],
+    [' ', '|', ' ', '_', '_', '_', ' ', '|', '|']
+  ]
+
+  function ocr(line) {
     line.pop();
-    var cols = this.colsForLine(line);
-    return this.translateCols(cols);
-  },
+    var cols = colsForLine(line);
+    return translateCols(cols);
+  }
 
-  colsForLine: function(line) {
+  function colsForLine(line) {
     var cols = [];
     var length = line[0].length;
     for(var i = 0; i < length; i++) {
@@ -37,18 +42,37 @@ module.exports = {
       }));
     }
     return cols;
-  },
+  }
 
-  translateCols: function(cols) {
+  function translateCols(cols) {
     var matrices = [];
     for(var i = 0, max = cols.length; i < max; i += 3) {
-      var matrix = cols.slice(i, i + 4);
-      matrices.push(this.translateMatrix(matrix));
+      var matrix = cols.slice(i, i + 3);
+      matrices.push(translateMatrix(matrix));
     }
     return matrices.join('');
-  },
-
-  translateMatrix: function(matrix) {
-    return Number(this.OCR_MATRICES.indexOf(matrix));
   }
-}
+
+  function translateMatrix(matrix) {
+    var matrices = OCR_MATRICES;
+    var match = '?';
+    matrices.forEach(function(candidate) {
+      if (matrix.flatten().equals(candidate)) {
+        match = matrices.indexOf(candidate);
+      }
+    });
+    return match;
+  }
+
+  module.exports = {
+    convert: function(ocrNumber) {
+      var split = ocrNumber.split('\n');
+      var converted = [];
+      for(var i = 0, max = split.length; i < max; i += 4) {
+        var line = split.slice(i, i + 4);
+        converted.push(ocr(line));
+      }
+      return converted.join(',');
+    }
+  };
+})();
